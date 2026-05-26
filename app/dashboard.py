74c9e9fd81fd0957397dfa271de9f6bb25fb477a
Paul Gálvez <paul.galvez@usm.cl>
2026-05-26 16:21:33 -0400

    Fix Streamlit Cloud import paths

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import plotly.graph_objects as go
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parents[1]))
 
 # -----------------------------------
 # FUNCIÓN SEMÁFORO
